2014c9a058f769ec9baec1c980873aa5cac4ce9b
3hyeonS <425269@yonsei.ac.kr>
2025-01-22 01:20:08 +0900

    test

--- src/auth/dto/sign-in-request.dto.ts
+++ src/auth/dto/sign-in-request.dto.ts
@@ -20,10 +20,4 @@ export class SignInRequestDto extends MemberEntity {
   @IsNotEmpty()
   @MaxLength(20)
   password: string;
-
-  // @IsNotEmpty()
-  // @IsEnum(['USER', 'CENTER', 'ADMIN'], {
-  //   message: 'Role은 USER, CENTER, 또는 ADMIN만 가능합니다.',
-  // })
-  // role: TRole;
 }
