8e4beb74829e2e5319f831567d2fb204cb8309ec
3hyeonS <425269@yonsei.ac.kr>
2025-02-27 03:42:10 +0900

    apple -11

--- src/auth/apple.strategy.ts
+++ src/auth/apple.strategy.ts
@@ -13,12 +13,12 @@ export class AppleStrategy extends PassportStrategy(Strategy, 'apple') {
       clientID: process.env.APPLE_CLIENT_ID,
       teamID: process.env.APPLE_TEAM_ID,
       keyID: process.env.APPLE_KEY_ID,
-      key: `——BEGIN PRIVATE KEY——
+      key: `-----BEGIN PRIVATE KEY-----
 MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgW/8iU0CT467JY4AP
 +6mkwnhgsE5NY/6q9T4YuJMW4EKgCgYIKoZIzj0DAQehRANCAAQhMFFv45d6kiVr
 tF3RYFhmtmzKGsD4qbw0TqioKHCNgrhxpdrTkqy684t3Nc+8NkbMmLVjwN0wiZSo
 7EkvhCO8
-——END PRIVATE KEY——`,
+-----END PRIVATE KEY-----`,
       // process.env.APPLE_PRIVATE_KEY_STRING?.replace(/\\n/g, '\n'),
       callbackURL: process.env.APPLE_CALLBACK_URL,
       passReqToCallback: false,
