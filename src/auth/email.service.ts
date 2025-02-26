f5857420ff83910973785b3064c6ab468dceddae
3hyeonS <425269@yonsei.ac.kr>
2025-02-26 23:11:11 +0900

    email change

--- src/auth/email.service.ts
+++ src/auth/email.service.ts
@@ -19,8 +19,8 @@ export class EmailService {
       port: 587, // TLS 전용 포트
       secure: false, // 연결 후 STARTTLS로 암호화
       auth: {
-        user: 'officefit19@gmail.com',
-        pass: 'kxnc mrik awsl ovgp',
+        user: 'oasisapp2025@gmail.com',
+        pass: 'aeid oadt cmcf acdv',
       },
       // tls: {
       //   rejectUnauthorized: false, // SSL 인증서 검증 무시
@@ -30,24 +30,24 @@ export class EmailService {
 
   async sendVerificationToEmail(email: string, code: string): Promise<void> {
     const emailOptions: EmailOptions = {
-      from: 'officefit19@gmail.com', // 보내는 사람 이메일 주소
+      from: 'oasisapp2025@gmail.com', // 보내는 사람 이메일 주소
       to: email, // 받는 사람의 이메일 주소
       subject: '가입 인증 메일',
       html: `
         <div style="font-family: Arial, sans-serif; max-width: 500px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9;">
           <h2 style="text-align: center; color: #333;">OfficeFit 인증 메일</h2>
           <p style="text-align: center; font-size: 16px; color: #555;">
-            안녕하세요, OfficeFit 인증 코드입니다.<br>
+            안녕하세요, oasis 인증 코드입니다.<br>
             아래 인증 코드를 입력하여 인증을 완료하세요.
           </p>
           <div style="text-align: center; padding: 15px; background: #007bff; color: #fff; font-size: 24px; font-weight: bold; border-radius: 5px;">
             ${code}
           </div>
           <p style="text-align: center; font-size: 14px; color: #777; margin-top: 15px;">
-            본 메일은 자동 발송되었습니다. 문의가 필요하시면 <a href="mailto:support@officefit.com" style="color: #007bff; text-decoration: none;">support@officefit.com</a>으로 연락주세요.
+            본 메일은 자동 발송되었습니다. 문의가 필요하시면 <a href="mailto:oasisapp2025@gmail.com" style="color: #007bff; text-decoration: none;">oasisapp2025@gmail.com</a>으로 연락주세요.
           </p>
           <p style="text-align: center; font-size: 12px; color: #aaa;">
-            ⓒ 2025 OfficeFit. All rights reserved.
+            ⓒ 2025 oasis. All rights reserved.
           </p>
         </div>
       `,
