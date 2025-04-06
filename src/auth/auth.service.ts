2c5c77bbca48fe8f7046788717e60572734ba40c
3hyeonS <425269@yonsei.ac.kr>
2025-04-06 16:34:23 +0900

    kakao4

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -376,8 +376,9 @@ export class AuthService {
     // 카카오 프로필 데이터를 기반으로 사용자 찾기 또는 생성 로직을 구현
     const existingUser = await this.userRepository.findOneBy({
       email: kakaoEmail,
+      signWith: await this.signWithRepository.findOneBy({ platform: 'KAKAO' }),
     });
-    if (existingUser?.kakaoKey.kakaoId == kakaoUserId) {
+    if (existingUser?.kakaoKey?.kakaoId == kakaoUserId) {
       return existingUser;
     }
 
