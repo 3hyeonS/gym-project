c39e977c9f40519bbfb0f14206d48c73ecfdd830
3hyeonS <425269@yonsei.ac.kr>
2025-04-08 01:01:34 +0900

    number

--- src/recruitment/dto/id-request-dto.ts
+++ src/recruitment/dto/id-request-dto.ts
@@ -5,7 +5,7 @@ export class IdRequestDto {
   @ApiProperty({
     type: Number,
     description: '공고의 id',
-    example: '1',
+    example: 1,
   })
   @IsNotEmpty()
   @IsNumber()
@@ -15,7 +15,7 @@ export class IdRequestDto {
     type: Number,
     description:
       '채용 중 여부(내 공고 1개 불러오기에서만 입력)  \n0: 채용 중  \n1: 만료',
-    example: '0',
+    example: 0,
   })
   @IsOptional()
   @IsNumber()
